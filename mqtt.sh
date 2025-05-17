15e518d22318239c7103a2c23edb032755c658cb
SioGabx <41244237+SioGabx@users.noreply.github.com>
2025-05-17 19:47:56 +0200

    changed

--- mqtt.sh
+++ mqtt.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/SioGabx/Proxmox/main/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # License: MIT
@@ -8,18 +8,18 @@ source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build
 function header_info {
 clear
 cat <<"EOF"
-    ____                                      _____                 
-   / __ \____ ____  ____ ___  ____  ____     / ___/__  ______  _____
-  / / / / __  / _ \/ __  __ \/ __ \/ __ \    \__ \/ / / / __ \/ ___/
- / /_/ / /_/ /  __/ / / / / / /_/ / / / /   ___/ / /_/ / / / / /__  
-/_____/\__,_/\___/_/ /_/ /_/\____/_/ /_/   /____/\__, /_/ /_/\___/  
-                                                /____/              
+    __  ___           ____        _ ____________    
+   /  |/  /___  _____/ __ \__  __(_)_  __/_  __/___ 
+  / /|_/ / __ \/ ___/ / / / / / / / / /   / / / __ \
+ / /  / / /_/ (__  ) /_/ / /_/ / / / /   / / / /_/ /
+/_/  /_/\____/____/\___\_\__,_/_/ /_/   /_/  \____/ 
+                                                    
 EOF
 }
 header_info
 echo -e "Loading..."
-APP="Daemon Sync"
-var_disk="8"
+APP="MQTT"
+var_disk="2"
 var_cpu="1"
 var_ram="512"
 var_os="debian"
@@ -54,11 +54,11 @@ function default_settings() {
 
 function update_script() {
 header_info
-if [[ ! -d /var ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
-msg_info "Updating $APP LXC"
+if [[ ! -f /etc/mosquitto/conf.d/default.conf ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+msg_info "Updating ${APP} LXC"
 apt-get update &>/dev/null
 apt-get -y upgrade &>/dev/null
-msg_ok "Updated $APP LXC"
+msg_ok "Updated Successfully"
 exit
 }
 
@@ -67,5 +67,3 @@ build_container
 description
 
 msg_ok "Completed Successfully!\n"
-echo -e "${APP} should be reachable by going to the following URL.
-         ${BL}http://${IP}:8084${CL} \n"
